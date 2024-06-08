4b6b606d455669d50114c50fe6d7ac55b3eb42b5
zentag <38821230+zentag@users.noreply.github.com>
2024-06-08 10:04:04 -0400

    autoupdate

--- home.nix
+++ home.nix
@@ -15,7 +15,7 @@
       update-sys="sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/";
       update-user="home-manager switch --flake ~/.zentag-nix-config/";
       update-lock="cd ~/.zentag-nix-config/ && nix flake update";
-      update="update-lock && update-sys && update-user && cd ~/.zentag-nix-config && git add . && git commit -m 'autoupdate' && git push origin";
+      update="olddir=$(pwd) && update-lock && update-sys && update-user && cd ~/.zentag-nix-config && git add . && git commit -m 'autoupdate' && git push origin && cd olddir";
       ff="fastfetch";
       fk="fuck";
     };
