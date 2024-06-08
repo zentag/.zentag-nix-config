54c929c8e711ed21a68d78e0614d874115c81f45
zentag <38821230+zentag@users.noreply.github.com>
2024-06-08 13:23:26 -0400

    add github shortcut aliases

--- home.nix
+++ home.nix
@@ -15,8 +15,14 @@
       update-user="home-manager switch --flake ~/.zentag-nix-config/";
       update-lock="cd ~/.zentag-nix-config/ && nix flake update";
       update="olddir=$(pwd) && update-lock && update-sys && update-user && cd ~/.zentag-nix-config && git add . && git commit -m 'autoupdate' && git push origin & cd $olddir";
-      home-config="nvim ~/.zentag-nix-config/home.nix && update-user && echo 'You should git commit now!!!'";
-      nix-config="nvim ~/.zentag-nix-config/configuration.nix && sudo nixos-rebuild switch --flake ~/.zentag-nix-config/ && echo 'You should git commit now!!!'";
+      home-config="nvim ~/.zentag-nix-config/home.nix && update-user && echo 'You should git commit now!!!' && source ~/.zshrc && cd ~/.zentag-nix-config";
+      nix-config="nvim ~/.zentag-nix-config/configuration.nix && sudo nixos-rebuild switch --flake ~/.zentag-nix-config/ && echo 'You should git commit now!!!' && cd ~/.zentag-nix-config/";
+      ga="git add .";
+      gp="git push origin";
+      gf="git pull";
+      gb="git branch";
+      gc="git checkout";
+      gd="git diff";
       ff="fastfetch";
       fk="fuck";
     };
