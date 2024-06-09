5f383f594adedabe166ff18b357c65b1085d2fe0
zentag <38821230+zentag@users.noreply.github.com>
2024-06-09 13:36:40 -0400

    format home flake to be nicer

--- home.nix
+++ home.nix
@@ -5,76 +5,79 @@
   # manage.
   home.username = "zen";
   home.homeDirectory = "/home/zen";
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      ls="eza --width=75 --hyperlink --color=always --git --icons=always";
-      cd="z";
-      kali="quickemu --vm kali-kali-weekly.conf --display spice";
-      update-sys="sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/";
-      update-user="home-manager switch --flake ~/.zentag-nix-config/";
-      update-lock="cd ~/.zentag-nix-config/ && nix flake update";
-      update="olddir=$(pwd) && update-lock && update-sys && update-user && cd ~/.zentag-nix-config && git add . && git commit -m 'autoupdate' && git push origin & cd $olddir";
-      home-config="nvim ~/.zentag-nix-config/home.nix && update-user && source ~/.zshrc && tput setaf 1; echo 'You should git commit now!!!' && cd ~/.zentag-nix-config";
-      nix-config="nvim ~/.zentag-nix-config/configuration.nix && sudo nixos-rebuild switch --flake ~/.zentag-nix-config/ && tput setaf 1; echo 'You should git commit now!!!' && cd ~/.zentag-nix-config/";
-      ga="git add";
-      gp="git push origin";
-      gf="git pull";
-      gb="git branch";
-      gc="git commit";
-      gch="git checkout";
-      gd="git diff";
-      galias="alias | grep -oE 'g[a-z]=.?*'";
-      ff="fastfetch";
-      fk="fuck";
+  programs = {  
+    zsh = {
+      enable = true;
+      shellAliases = {
+        ls="eza --width=75 --hyperlink --color=always --git --icons=always";
+        cd="z";
+        kali="quickemu --vm kali-kali-weekly.conf --display spice";
+        update-sys="sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/";
+        update-user="home-manager switch --flake ~/.zentag-nix-config/";
+        update-lock="cd ~/.zentag-nix-config/ && nix flake update";
+        update="olddir=$(pwd) && update-lock && update-sys && update-user && cd ~/.zentag-nix-config && git add . && git commit -m 'autoupdate' && git push origin & cd $olddir";
+        home-config="nvim ~/.zentag-nix-config/home.nix && update-user && source ~/.zshrc && tput setaf 1; echo 'You should git commit now!!!' && cd ~/.zentag-nix-config";
+        nix-config="nvim ~/.zentag-nix-config/configuration.nix && sudo nixos-rebuild switch --flake ~/.zentag-nix-config/ && tput setaf 1; echo 'You should git commit now!!!' && cd ~/.zentag-nix-config/";
+        ga="git add";
+        gp="git push origin";
+        gf="git pull";
+        gb="git branch";
+        gc="git commit";
+        gch="git checkout";
+        gd="git diff";
+        galias="alias | grep -oE 'g[a-z]=.?*'";
+        ff="fastfetch";
+        fk="fuck";
+      };
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      plugins = [
+        {
+          name = "powerlevel10k-config";
+          src = ./p10k;
+          file = "p10k.zsh";
+        }
+        {
+          name = "zsh-powerlevel10k";
+          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+          file = "powerlevel10k.zsh-theme";
+        }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+        {
+          name = "fzf-git-sh";
+          file = "fzf-git.sh";
+          src = pkgs.fetchFromGitHub {
+            owner = "junegunn";
+            rev = "bd8ac4ba4c9d7d12b34f7fa2b0d334f50cdb5254";
+            repo = "fzf-git.sh";
+            sha256 = "sha256-ZYgov/P7fcB1Zjj5UMVbr7+bjRKLwzpqddHBOCNd+RQ=";
+          };
+        }
+      ];
     };
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "powerlevel10k-config";
-        src = ./p10k;
-        file = "p10k.zsh";
-      }
-      {
-        name = "zsh-powerlevel10k";
-        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-        file = "powerlevel10k.zsh-theme";
-      }
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
-      }
-      {
-        name = "fzf-git-sh";
-        file = "fzf-git.sh";
-        src = pkgs.fetchFromGitHub {
-          owner = "junegunn";
-          rev = "bd8ac4ba4c9d7d12b34f7fa2b0d334f50cdb5254";
-          repo = "fzf-git.sh";
-          sha256 = "sha256-ZYgov/P7fcB1Zjj5UMVbr7+bjRKLwzpqddHBOCNd+RQ=";
-        };
-      }
-    ];
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    thefuck = {
+      enable = true;
+      enableZshIntegration = true;
+    };
   };
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.thefuck = {
-    enable = true;
-    enableZshIntegration = true;
-  };
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
