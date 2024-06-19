a77f7d14eba98a0b2a05425c51510638a5412dc3
zentag <38821230+zentag@users.noreply.github.com>
2024-06-19 10:51:19 -0400

    refactor for better modularity

---
+++ home/dconf.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  dconf = {
+    enable = true;
+    settings = { 
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          blur-my-shell.extensionUuid
+          burn-my-windows.extensionUuid
+          clipboard-history.extensionUuid
+          gsconnect.extensionUuid
+          noannoyance-fork.extensionUuid
+          tiling-assistant.extensionUuid
+          vitals.extensionUuid
+        ];
+      };
+      "org/gnome/epiphany/web" = {
+        enable-webextensions = true;
+      };
+    };
+  };
+  
+}
---
+++ home/programs.nix
@@ -0,0 +1,77 @@
+{ pkgs, ... }:
+{
+  programs = {  
+    zsh = {
+      enable = true;
+      shellAliases = {
+        cd="z";
+        ff="fastfetch";
+        fk="fuck";
+        ga="git add";
+        galias="alias | grep -oE 'g[a-z][a-z]?=.?*'";
+        gb="git branch";
+        gc="git commit";
+        gch="git checkout";
+        gd="git diff";
+        gf="git pull";
+        gp="git push origin";
+        gs="git status";
+        home-config="nvim ~/.zentag-nix-config/home.nix && update-user && source ~/.zshrc && tput setaf 1; echo 'You should git commit now!!!' && cd ~/.zentag-nix-config";
+        kali="quickemu --vm kali-kali-weekly.conf --display spice";
+        ls="eza --width=75 --hyperlink --color=always --git --icons=always";
+        nix-config="nvim ~/.zentag-nix-config/configuration.nix && sudo nixos-rebuild switch --flake ~/.zentag-nix-config/ && tput setaf 1; echo 'You should git commit now!!!' && cd ~/.zentag-nix-config/";
+        update-lock="cd ~/.zentag-nix-config/ && nix flake update";
+        update-sys="sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/";
+        update-user="home-manager switch --flake ~/.zentag-nix-config/";
+        update="olddir=$(pwd) && update-lock && update-sys && update-user && cd ~/.zentag-nix-config && git add . && git commit -m 'autoupdate' && git push origin & cd $olddir";
+      };
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      plugins = [
+        {
+          name = "powerlevel10k-config";
+          src = ./p10k;
+          file = "p10k.zsh";
+        }
+        {
+          name = "zsh-powerlevel10k";
+          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+          file = "powerlevel10k.zsh-theme";
+        }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+        {
+          name = "fzf-git-sh";
+          file = "fzf-git.sh";
+          src = pkgs.fetchFromGitHub {
+            owner = "junegunn";
+            rev = "bd8ac4ba4c9d7d12b34f7fa2b0d334f50cdb5254";
+            repo = "fzf-git.sh";
+            sha256 = "sha256-ZYgov/P7fcB1Zjj5UMVbr7+bjRKLwzpqddHBOCNd+RQ=";
+          };
+        }
+      ];
+    };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    thefuck = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
+  
+}
