732e3a2751ac7cb5a14b6cf032549c7447e76c9d
zentag <38821230+zentag@users.noreply.github.com>
2024-08-21 08:19:58 -0400

    start sh add home manager

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
-nix-shell -p gnome.tali --command "tali"
-tali
+nix-shell -p git --command "git clone https://github.com/zentag/.zentag-nix-config.git"
+cd .zentag-nix-config
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+nix-shell -p git --command "sudo nixos-rebuild switch --flake ."
