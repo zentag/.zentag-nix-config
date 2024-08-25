cf03ae7395189fff8b81c842d0d7e6e242e7c3ce
zentag <38821230+zentag@users.noreply.github.com>
2024-08-25 08:17:39 -0400

    delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-nix-shell -p git --command "git clone https://github.com/zentag/.zentag-nix-config.git"
-cd .zentag-nix-config
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
-nix-shell -p git --command "sudo nixos-rebuild switch --flake ."
