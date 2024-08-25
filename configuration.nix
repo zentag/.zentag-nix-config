22672611d75414a5e401d7997bcc909512a231d2
zentag <38821230+zentag@users.noreply.github.com>
2024-08-25 08:46:57 -0400

    remove weird fix

--- configuration.nix
+++ configuration.nix
@@ -10,10 +10,6 @@
       ./hardware-configuration.nix
     ];
 
-  # weird fix
-  systemd.network.wait-online.enable = false;
-
-  boot.initrd.systemd.network.wait-online.enable = false;
   # nix search wget
   environment.systemPackages = with pkgs; [
     bat
