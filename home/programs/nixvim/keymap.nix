d1987fe61601e3b807809ecd4ee4b1a00170c14d
zentag <38821230+zentag@users.noreply.github.com>
2024-12-06 19:23:23 -0500

    reorganize

---
+++ home/programs/nixvim/keymap.nix
@@ -0,0 +1,28 @@
+{
+  programs.nixvim = {
+    keymaps = [
+      {
+        action = "<cmd>Trouble<CR>";
+        key = "<C-t>";
+      }
+      {
+        mode = "n";
+        key = "<leader>db";
+        action = ":DapToggleBreakpoint<cr>";
+        options = {
+          silent = true;
+          desc = "Toggle Breakpoint";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>dc";
+        action = ":DapContinue<cr>";
+        options = {
+          silent = true;
+          desc = "Continue";
+        };
+      }
+    ];
+  };
+}
