cb15f53e2ed66e3673eb33b39defa353dc06cc1a
zentag <38821230+zentag@users.noreply.github.com>
2025-04-19 19:33:58 -0400

    Add git commit to helper script

--- helpers/config.sh
+++ helpers/config.sh
@@ -2,8 +2,6 @@ select mode in $(echo "ConfigureHM ConfigureNixos Both"); do
 	nvim ~/.zentag-nix-config/
 	if [ "$REPLY" = 1 ]; then
 		home-manager switch --flake ~/.zentag-nix-config/
-
-		source ~/.zshrc
 	fi
 	if [ "$REPLY" = 2 ]; then
 		sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/
@@ -12,8 +10,9 @@ select mode in $(echo "ConfigureHM ConfigureNixos Both"); do
 	if [ "$REPLY" = 3 ]; then
 		sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/
 		home-manager switch --flake ~/.zentag-nix-config/
-		source ~/.zshrc
 	fi
-	tput setaf 1
-	echo 'You should git commit now!!!'
+	read -rp "Name of git commit (or enter for no commit): " name
+	git -C ~/.zentag-nix-config/ add .
+	git -C ~/.zentag-nix-config/ commit -m "$name"
+	git -C ~/.zentag-nix-config/ push origin
 done
