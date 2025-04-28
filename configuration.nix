3c40ad6d78f08ea75b77f8cab0b74a96c3fe3ed1
zentag <38821230+zentag@users.noreply.github.com>
2025-04-28 11:46:59 -0400

    refactor configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,19 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-{ config
-, pkgs
-, ...
-}: {
+{ pkgs, ... }: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # weird fix
-  systemd.network.wait-online.enable = false;
-
-  boot.initrd.systemd.network.wait-online.enable = false;
   # nix search wget
   environment.systemPackages = with pkgs; [
     bat
@@ -55,36 +45,38 @@
     kernel.packages = pkgs.linuxPackages-rt_latest;
     rtirq.enable = true;
   };
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  hardware.enableAllFirmware = true;
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
+  hardware = {
+    enableAllFirmware = true;
 
-  networking.hostName = "zens-good-laptop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.wireless.iwd.enable = true;
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+  };
+  networking = {
+    hostName = "zens-good-laptop"; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    wireless.iwd.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  networking.networkmanager.wifi.backend = "iwd";
+    # Enable networking
+    networkmanager.enable = true;
+    networkmanager.wifi.backend = "iwd";
 
-  networking.firewall.enable = false;
-
+    firewall.enable = false;
+  };
   # Set your time zone.
   time.timeZone = "America/Indiana/Indianapolis";
 
@@ -102,71 +94,71 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+  services = {
+    flatpak.enable = true;
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
 
-  services.flatpak.enable = true;
+      # Enable the GNOME Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+      # Configure keymap in X11
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+      xkb = { layout = "us"; };
+    };
 
-  # Configure keymap in X11
-  services.xserver = {
-    xkb = { layout = "us"; };
-  };
+    xrdp = {
+      enable = true;
+      openFirewall = true;
+      defaultWindowManager = "gnome-remote-desktop";
+    };
 
-  services.xrdp = {
-    enable = true;
-    openFirewall = true;
-    defaultWindowManager = "gnome-remote-desktop";
-  };
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
+    tailscale = {
+      enable = true;
+    };
 
-  services.tailscale = {
-    enable = true;
-  };
+    openssh = {
+      enable = true;
+      ports = [ 22 ];
+      settings = {
+        PasswordAuthentication = true;
+        AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+        UseDns = true;
+        X11Forwarding = false;
+        PermitRootLogin = "prohibit-password";
+      };
+    };
+    upower.ignoreLid = true;
 
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings = {
-      PasswordAuthentication = true;
-      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-      UseDns = true;
-      X11Forwarding = false;
-      PermitRootLogin = "prohibit-password";
+    # Enable sound with pipewire.
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = false;
+      # If you want to use JACK applications, uncomment this
+      jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
     };
   };
-  virtualisation.docker.enable = true;
-  services.upower.ignoreLid = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = false;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
+  virtualisation.docker.enable = true;
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users = {
@@ -210,10 +202,10 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  # firewall.allowedTCPPorts = [ ... ];
+  # firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
