63489d892b1267b14e1a0ae2a6070de23f114f7e
zentag <38821230+zentag@users.noreply.github.com>
2025-06-09 20:15:17 -0400

    remove java stuff I don't need

--- home/programs/nixvim/keymap.nix
+++ home/programs/nixvim/keymap.nix
@@ -5,24 +5,6 @@
         action = "<cmd>Trouble diagnostics<CR>";
         key = "<C-t>";
       }
-      {
-        mode = "n";
-        key = "<leader>db";
-        action = ":DapToggleBreakpoint<cr>";
-        options = {
-          silent = true;
-          desc = "Toggle Breakpoint";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>dc";
-        action = ":DapContinue<cr>";
-        options = {
-          silent = true;
-          desc = "Continue";
-        };
-      }
     ];
   };
 }
