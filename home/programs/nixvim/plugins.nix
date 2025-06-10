d5baa392e83294b3237e0d319bb1312bebc0b185
zentag <38821230+zentag@users.noreply.github.com>
2025-06-09 20:31:27 -0400

    remove copilot emoji

--- home/programs/nixvim/plugins.nix
+++ home/programs/nixvim/plugins.nix
@@ -131,9 +131,6 @@
       };
       lspkind = {
         enable = true;
-        symbolMap = {
-          Copilot = "";
-        };
         extraOptions = {
           maxwidth = 50;
           ellipsis_char = "...";
