69c5d11a21f7ba2653fd4bb2bcc74e0ede8ca4d5
zentag <38821230+zentag@users.noreply.github.com>
2025-06-18 16:20:24 -0400

    add gnome enable option

--- home/dconf.nix
+++ home/dconf.nix
@@ -1,19 +1,30 @@
-{ pkgs, ... }: {
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = with pkgs.gnomeExtensions; [
-          blur-my-shell.extensionUuid
-          caffeine.extensionUuid
-          gsconnect.extensionUuid
-          tiling-assistant.extensionUuid
-          vitals.extensionUuid
-        ];
-      };
-      "org/gnome/epiphany/web" = {
-        enable-webextensions = true;
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    gnome.enable = lib.mkEnableOption "Enables gnome desktop environment";
+  };
+  config = lib.mkIf config.gnome.enable {
+    dconf = {
+      enable = true;
+      settings = {
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = with pkgs.gnomeExtensions; [
+            blur-my-shell.extensionUuid
+            caffeine.extensionUuid
+            gsconnect.extensionUuid
+            tiling-assistant.extensionUuid
+            vitals.extensionUuid
+          ];
+        };
+        "org/gnome/epiphany/web" = {
+          enable-webextensions = true;
+        };
       };
     };
   };
