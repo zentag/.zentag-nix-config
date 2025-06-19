369987cce24c9b4d6e65df262f313290944bfea2
zentag <38821230+zentag@users.noreply.github.com>
2025-06-18 20:27:18 -0400

    remove xserver which was there for some reason?

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -93,17 +93,6 @@
     # Enable the GNOME Desktop Environment.
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
-    xserver = {
-      # Enable the X11 windowing system.
-      enable = true;
-
-      # Configure keymap in X11
-
-      xkb = {
-        layout = "us";
-      };
-    };
-
     xrdp = {
       enable = true;
       openFirewall = true;
