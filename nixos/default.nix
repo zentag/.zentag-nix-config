6e15a80a38b027b5440e24bd8cb92df2119b83fa
zentag <38821230+zentag@users.noreply.github.com>
2025-08-28 13:22:19 -0400

    q

--- nixos/default.nix
+++ nixos/default.nix
@@ -4,7 +4,17 @@
     ./configuration.nix
     ./laptop.nix
   ];
-
+  systemd.services.wpa_supplicant.environment.OPENSSL_CONF = pkgs.writeText "openssl.cnf" ''
+    openssl_conf = openssl_init
+    [openssl_init]
+    ssl_conf = ssl_sect
+    [ssl_sect]
+    system_default = system_default_sect
+    [system_default_sect]
+    Options = UnsafeLegacyRenegotiation
+    [system_default_sect]
+    CipherString = Default:@SECLEVEL=0
+  '';
   options.customOpts = {
     laptop = lib.mkEnableOption "Is this device a laptop";
   };
