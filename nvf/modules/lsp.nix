4d2c282d0e517950fbad7df05ec0abd83318d1dc
zentag <38821230+zentag@users.noreply.github.com>
2025-11-14 13:55:26 -0500

    add language support for what i use"

--- nvf/modules/lsp.nix
+++ nvf/modules/lsp.nix
@@ -12,6 +12,17 @@
       # Nix language and diagnostics.
       nix.enable = true;
       ts.enable = true;
+      astro.enable = true;
+      css.enable = true;
+      tailwind.enable = true;
+      html.enable = true;
+      markdown.enable = true;
+      bash.enable = true;
+      clang.enable = true;
+      yaml.enable = true;
+      go.enable = true;
+      lua.enable = true;
+      rust.enable = true;
     };
   };
 }
