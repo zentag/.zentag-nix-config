9b55cf533f6606a2323bef7b35a9b089021d6946
zentag <38821230+zentag@users.noreply.github.com>
2025-11-16 00:25:52 -0500

    enable markview

--- nvf/modules/lsp.nix
+++ nvf/modules/lsp.nix
@@ -11,19 +11,22 @@
       enableExtraDiagnostics = true;
 
       # Nix language and diagnostics.
-      nix.enable = true;
-      ts.enable = true;
       astro.enable = true;
-      css.enable = true;
-      tailwind.enable = true;
-      html.enable = true;
-      markdown.enable = true;
       bash.enable = true;
       clang.enable = true;
-      yaml.enable = true;
+      css.enable = true;
       go.enable = true;
+      html.enable = true;
       lua.enable = true;
+      markdown = {
+        enable = true;
+        extensions.markview-nvim.enable = true;
+      };
+      nix.enable = true;
       rust.enable = true;
+      tailwind.enable = true;
+      ts.enable = true;
+      yaml.enable = true;
     };
   };
 }
