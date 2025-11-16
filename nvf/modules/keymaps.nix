25b06127384c80c43bb2f21c5bed1d6f81392012
zentag <38821230+zentag@users.noreply.github.com>
2025-11-16 00:12:42 -0500

    fix keymaps

--- nvf/modules/keymaps.nix
+++ nvf/modules/keymaps.nix
@@ -3,22 +3,27 @@
     {
       action = "<cmd>Trouble diagnostics<CR>";
       key = "<leader>t";
+      mode = "n";
     }
     {
       action = "<cmd>TodoQuickFix<CR>";
       key = "<leader>td";
+      mode = "n";
     }
     {
       key = "<leader>f";
       action = "<cmd>Pick files<CR>";
+      mode = "n";
     }
     {
       key = "<leader>g";
       action = "<cmd>Pick live-grep<CR>";
+      mode = "n";
     }
     {
       key = "<leader>e";
       action = "<cmd>lua MiniFiles.open()<CR>";
+      mode = "n";
     }
   ];
 }
