88d578043cbb952161b7cf0490a807adb83df5c3
zentag <38821230+zentag@users.noreply.github.com>
2025-11-28 15:46:19 -0500

    add pulse compatability

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -134,7 +134,7 @@
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
-      pulse.enable = false;
+      pulse.enable = true;
       # If you want to use JACK applications, uncomment this
       jack.enable = true;
 
