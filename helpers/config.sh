ececdcacce362458b1d132f4f34124bdd9efa1eb
zentag <38821230+zentag@users.noreply.github.com>
2026-04-30 15:14:46 -0400

    change config script so file explorer is in correct dir, ignore warnings

--- helpers/config.sh
+++ helpers/config.sh
@@ -1,21 +1,27 @@
-select mode in $(echo "ConfigureHM ConfigureNixos Both"); do
-	nvim ~/.zentag-nix-config/
-	git -C ~/.zentag-nix-config/ add .
-	if [ "$REPLY" = 1 ]; then
-		home-manager switch --flake ~/.zentag-nix-config/
-	fi
-	if [ "$REPLY" = 2 ]; then
-		sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/
+#!/usr/bin/env bash
+# disable unused warning for mode (we use the number from $REPLY instead)
+# shellcheck disable=SC2034
+select mode in "Configure HM" "Configure Nixos" "Both"; do
+        cd ~/.zentag-nix-config/ || exit
+        nvim .
+        git -C ~/.zentag-nix-config/ add .
+        if [ "$REPLY" = 1 ]; then
+                home-manager switch --flake ~/.zentag-nix-config/
+        fi
+        if [ "$REPLY" = 2 ]; then
+                sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/
 
-	fi
-	if [ "$REPLY" = 3 ]; then
-		sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/
-		home-manager switch --flake ~/.zentag-nix-config/
-	fi
-	read -rp "Name of git commit (or enter for no commit): " name
-	if [ "$name" != "" ]; then
-		git -C ~/.zentag-nix-config/ commit -m "$name"
-		git -C ~/.zentag-nix-config/ push origin
-	fi
-	source ~/.zshrc &>/dev/null
+        fi
+        if [ "$REPLY" = 3 ]; then
+                sudo nixos-rebuild switch --upgrade --flake ~/.zentag-nix-config/
+                home-manager switch --flake ~/.zentag-nix-config/
+        fi
+        read -rp "Name of git commit (or enter for no commit): " name
+        if [ "$name" != "" ]; then
+                git -C ~/.zentag-nix-config/ commit -m "$name"
+                git -C ~/.zentag-nix-config/ push origin
+        fi
+        # this gets rid of shellcheck warning that it can't follow the new source
+        # shellcheck disable=SC1090
+        source ~/.zshrc &>/dev/null
 done
