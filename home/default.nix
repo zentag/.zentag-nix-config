8f5c340b9760298ab23c4476c09cd9742e986211
zentag <38821230+zentag@users.noreply.github.com>
2026-05-10 11:52:57 -0400

    add termfilechooser

--- home/default.nix
+++ home/default.nix
@@ -98,7 +98,7 @@ in {
       )
       ++ [
         bottom
-        obsidian
+        xdg-desktop-portal-termfilechooser
         # # It is sometimes useful to fine-tune packages, for example, by applying
         # # overrides. You can do that directly here, just don't forget the
         # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
