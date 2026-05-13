3440257bdd34241bc3a16aee0b791dfb946eaa7f
zentag <38821230+zentag@users.noreply.github.com>
2026-05-12 20:28:11 -0400

    make neovim script cd to dir

--- helpers/neovim.sh
+++ helpers/neovim.sh
@@ -2,5 +2,5 @@
 if [ "$#" -eq 0 ]; then
         hyprctl dispatch exec "[workspace 2] ghostty -e nvim $(pwd)"
 else
-        hyprctl dispatch exec "[workspace 2] ghostty -e nvim $1"
+        hyprctl dispatch exec "[workspace 2] ghostty --working-directory=$1 -e nvim $1"
 fi
