2224e1f42fec61238400e2ba1af926a7bab93457
zentag <38821230+zentag@users.noreply.github.com>
2026-05-14 13:47:23 -0400

    make scripts resistant to errors and also less annoying

--- helpers/config.sh
+++ helpers/config.sh
@@ -3,7 +3,11 @@
 # shellcheck disable=SC2034
 select mode in "Configure HM" "Configure Nixos" "Both"; do
         cd ~/.zentag-nix-config/ || exit
-        ~/.zentag-nix-config/helpers/neovim.sh ~/.zentag-nix-config
+        if ! hyprctl clients | grep /home/zen/.zentag-nix-config; then
+                ~/.zentag-nix-config/helpers/neovim.sh ~/.zentag-nix-config
+        else
+                hyprctl dispatch focuswindow title:/home/zen/.zentag-nix-config
+        fi
         sleep 2
         while true; do
                 if [ "$(hyprctl activeworkspace | head -n 1 | cut -c 14)" = "1" ]; then
@@ -13,16 +17,21 @@ select mode in "Configure HM" "Configure Nixos" "Both"; do
         done
 
         git add .
-        if [ "$REPLY" = 1 ]; then
-                home-manager switch --flake ~/.zentag-nix-config/
+        errored=false
+        if [ "$REPLY" = 1 ] || [ "$REPLY" = 3 ]; then
+                if ! home-manager switch --flake ~/.zentag-nix-config/; then
+                        errored=true
+                fi
         fi
-        if [ "$REPLY" = 2 ]; then
-                sudo nixos-rebuild switch --flake ~/.zentag-nix-config/
-
+        if [ "$REPLY" = 2 ] || [ "$REPLY" = 3 ]; then
+                if ! sudo nixos-rebuild switch --flake ~/.zentag-nix-config/; then
+                        errored=true
+                fi
         fi
-        if [ "$REPLY" = 3 ]; then
-                sudo nixos-rebuild switch --flake ~/.zentag-nix-config/
-                home-manager switch --flake ~/.zentag-nix-config/
+        if $errored; then
+                echo hi
+                ~/.zentag-nix-config/helpers/config.sh
+                exit
         fi
         read -rp "Name of git commit (or enter for no commit): " name
         if [ "$name" != "" ]; then
--- helpers/neovim.sh
+++ helpers/neovim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$#" -eq 0 ]; then
-        hyprctl dispatch exec "[workspace 2] ghostty --working-directory=$(pwd)-e nvim $(pwd)"
+        hyprctl dispatch exec "[workspace 2] ghostty --working-directory=$(pwd) --title=$1 -e nvim $(pwd)"
 else
-        hyprctl dispatch exec "[workspace 2] ghostty --working-directory=$1 -e nvim $1"
+        hyprctl dispatch exec "[workspace 2] ghostty --working-directory=$1 --title=$1 -e nvim $1"
 fi
