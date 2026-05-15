d1327fc31c49181dd3825beb54f35bced9f6c24a
zentag <38821230+zentag@users.noreply.github.com>
2026-05-15 14:06:37 -0400

    refactor dap

--- nvf/dap.nix
+++ nvf/dap.nix
@@ -1,12 +1,13 @@
 {pkgs, ...}: {
-  vim.debugger.nvim-dap = {
-    enable = true;
-    ui.enable = true;
-  };
-  vim.extraPlugins = with pkgs.vimPlugins; {
-    virtualtext = {
-      setup = "require('nvim-dap-virtual-text').setup()";
-      package = nvim-dap-virtual-text;
+  vim = {
+    debugger.nvim-dap = {
+      enable = true;
+    };
+    extraPlugins = with pkgs.vimPlugins; {
+      virtualtext = {
+        setup = "require('nvim-dap-virtual-text').setup()";
+        package = nvim-dap-virtual-text;
+      };
     };
   };
 }
