01b210c027e9d002e365cd3d204c76c0461a3ba3
zentag <38821230+zentag@users.noreply.github.com>
2026-05-15 13:10:56 -0400

    add virtualtext to dap

--- nvf/dap.nix
+++ nvf/dap.nix
@@ -1,6 +1,12 @@
-{
+{pkgs, ...}: {
   vim.debugger.nvim-dap = {
     enable = true;
     ui.enable = true;
+  };
+  vim.extraPlugins = with pkgs.vimPlugins; {
+    virtualtext = {
+      setup = "require('nvim-dap-virtual-text').setup()";
+      package = nvim-dap-virtual-text;
+    };
   };
 }
