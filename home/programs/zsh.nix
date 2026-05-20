72bc1dbb8fc1e05d199632da8fe788260814bcad
zentag <38821230+zentag@users.noreply.github.com>
2026-05-19 21:51:10 -0400

    add direnv

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -10,8 +10,14 @@
       default = true;
     };
   };
+
   config = lib.mkIf config.customOpts.zsh.enable {
     programs = {
+      direnv = {
+        enable = true;
+        enableZshIntegration = true; # see note on other shells below
+        nix-direnv.enable = true;
+      };
       zsh = {
         enable = true;
         shellAliases = {
