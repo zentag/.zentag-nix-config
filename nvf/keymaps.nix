fdc723ae1e333847c63c7e18b865390cae5fe2f2
zentag <38821230+zentag@users.noreply.github.com>
2026-06-08 12:48:36 -0400

    add telescope bindings

--- nvf/keymaps.nix
+++ nvf/keymaps.nix
@@ -12,12 +12,22 @@
     }
     {
       key = "<leader>f";
-      action = "<cmd>lua require('telescope.builtin').find_files<CR>";
+      action = "<cmd>lua require('telescope.builtin').find_files()<CR>";
       mode = "n";
     }
     {
       key = "<leader>g";
-      action = "<cmd>lua require('telescope.builtin').live_grep<CR>";
+      action = "<cmd>lua require('telescope.builtin').live_grep()<CR>";
+      mode = "n";
+    }
+    {
+      key = "<leader>ws";
+      action = "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>";
+      mode = "n";
+    }
+    {
+      key = "<leader>ds";
+      action = "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>";
       mode = "n";
     }
     {
