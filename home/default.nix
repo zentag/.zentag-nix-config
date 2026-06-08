75ea0257b2ac335e0bd3dba8c200a3de277b8f50
zentag <38821230+zentag@users.noreply.github.com>
2026-06-08 13:58:02 -0400

    remove line that caused warning

--- home/default.nix
+++ home/default.nix
@@ -153,8 +153,6 @@ in {
       EDITOR = "nvim";
     };
 
-    nixpkgs.config.allowUnfree = true;
-
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
   };
