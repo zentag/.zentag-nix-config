c2e46354319b603a19c7c71838712ce41e0068fa
zentag <38821230+zentag@users.noreply.github.com>
2026-06-20 08:58:38 -0400

    add workspace to obsidian.nvim

--- nvf/completion.nix
+++ nvf/completion.nix
@@ -6,6 +6,16 @@
       snippets.enable = true;
       surround.enable = true;
     };
-    notes.obsidian.enable = true;
+    notes.obsidian = {
+      enable = true;
+      setupOpts = {
+        workspaces = [
+          {
+            name = "notes";
+            path = "~/Documents/notes";
+          }
+        ];
+      };
+    };
   };
 }
