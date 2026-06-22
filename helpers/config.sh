4573092d3e98a9b5c25aee63392fe9832bc5d7f6
zentag <38821230+zentag@users.noreply.github.com>
2026-06-22 11:00:12 -0400

    update config.sh

--- helpers/config.sh
+++ helpers/config.sh
@@ -2,32 +2,30 @@
 # disable unused warning for mode (we use the number from $REPLY instead)
 # shellcheck disable=SC2034
 cd ~/.zentag-nix-config/ || exit
-if ! hyprctl clients | grep /home/zen/.zentag-nix-config >/dev/null; then
-        ~/.zentag-nix-config/helpers/neovim.sh ~/.zentag-nix-config
+if ! hyprctl clients | grep . >/dev/null; then
+        ./helpers/neovim.sh .
 else
-        hyprctl dispatch focuswindow title:/home/zen/.zentag-nix-config
+        hyprctl dispatch focuswindow title:"$(pwd)"
 fi
 sleep 2
 while true; do
         if [ "$(hyprctl activeworkspace | head -n 1 | cut -c 14)" = "1" ]; then
-                configuring=false
                 break
         fi
 done
 
 git add .
-# TODO: make this change for each system
-if ! sudo nixos-rebuild switch --flake ~/.zentag-nix-config#hp; then
+if ! sudo nixos-rebuild switch --flake .#"$(cat system.txt)"; then
         read -rp $'\nPress enter to rerun script'
-        ~/.zentag-nix-config/helpers/config.sh
+        ./helpers/config.sh
         exit
 fi
 read -rp "Name of git commit (or enter for no commit): " name
 if [ "$name" != "" ]; then
-        git -C ~/.zentag-nix-config/ commit -m "$name"
-        git -C ~/.zentag-nix-config/ push origin
+        git commit -m "$name"
+        git push origin
 else
-        ~/.zentag-nix-config/helpers/config.sh
+        ./helpers/config.sh
         exit
 fi
 # this gets rid of shellcheck warning that it can't follow the new source
