c1514d369e5d6dabd2cdb64ee5bf0719886b67ba
zentag <38821230+zentag@users.noreply.github.com>
2026-06-25 21:16:18 -0400

    add java to system via design feature

--- modules/features/design.nix
+++ modules/features/design.nix
@@ -1,15 +1,20 @@
 {self, ...}: {
-  flake.nixosModules.design = {config, ...}: {
+  flake.nixosModules.design = {
+    config,
+    pkgs,
+    ...
+  }: {
     imports = [
       (self.lib.hm config.username "design")
     ];
+    programs.java.enable = true;
+    programs.java.package = pkgs.jdk25;
   };
   flake.homeModules.design = {pkgs, ...}: {
     home.packages = with pkgs; [
       cura-appimage
       freecad
-      (pkgs.kicad.override {withJava = true;})
-
+      kicad
       prusa-slicer
     ];
   };
